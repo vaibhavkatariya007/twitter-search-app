59c251c4a2455e32aa344eec2c72a5561d8a2149
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-07-30 22:34:44 +0530

    Updates

---
+++ precache-manifest.fbf1bd07d591303b65243346be734a07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b1f108b8e8c0fe4801899734898e82",
+    "url": "/twitter-search-app/index.html"
+  },
+  {
+    "revision": "63cd8905bedd4437e778",
+    "url": "/twitter-search-app/static/css/2.951c421b.chunk.css"
+  },
+  {
+    "revision": "65cfcaf834353930bd3e",
+    "url": "/twitter-search-app/static/css/main.3493bdeb.chunk.css"
+  },
+  {
+    "revision": "63cd8905bedd4437e778",
+    "url": "/twitter-search-app/static/js/2.c5469610.chunk.js"
+  },
+  {
+    "revision": "b21c0b070fbf389a149e773557d68a8a",
+    "url": "/twitter-search-app/static/js/2.c5469610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5803a4b6a50d4d8f491",
+    "url": "/twitter-search-app/static/js/3.83731c42.chunk.js"
+  },
+  {
+    "revision": "65cfcaf834353930bd3e",
+    "url": "/twitter-search-app/static/js/main.3d95fef6.chunk.js"
+  },
+  {
+    "revision": "60d7c8d246dabf48e0c0",
+    "url": "/twitter-search-app/static/js/runtime-main.8d089fc9.js"
+  }
+]);
